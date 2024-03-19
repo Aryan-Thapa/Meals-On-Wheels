18b6c02fc3898a1b2b0555947e381c35e69c280a
Aryan-Thapa <aryanthapa219@gmail.com>
2024-03-19 16:16:22 +0530

    started working on firebase authentication

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
@@ -11,7 +11,39 @@ import {
 } from "@expo-google-fonts/oswald";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { initializeApp } from "firebase/app";
+import { FirebaseApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN1aqJ-JhR-b_DNuuqJsKMI7mDzVXLAN8",
+  authDomain: "meals-on-wheels-9317f.firebaseapp.com",
+  projectId: "meals-on-wheels-9317f",
+  storageBucket: "meals-on-wheels-9317f.appspot.com",
+  messagingSenderId: "581735937926",
+  appId: "1:581735937926:web:aada17873d4dffdfb0de07",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, "aryan@gmail.com", "test123")
+        .then((userCredential) => {
+          // User successfully signed in
+          setIsAuthenticated(true);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    }, 2000);
+  }, []);
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -25,7 +57,7 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-      <FavouritesContextProvider>
+        <FavouritesContextProvider>
           <LocationContextProvider>
             <RestaurantsContextProvider>
               <Navigation />
